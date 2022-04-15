aa5104b8672c0f130c50c0c09535e534f0767fe7
kenkomu <komukenneth216@gmail.com>
2022-04-15 02:16:46 -0700

    1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -15,7 +15,7 @@ int main(void)
 {
         int n;
         int last;
-        srand(timiie(0))
+        srand(time(0));
         n = rand() - RAND_MAX / 2;
         last = n % 10;
         if (last > 5)
