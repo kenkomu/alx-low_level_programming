332e1c4c9e5856c18c923958b13ce197592f0f0f
kenkomu <komukenneth216@gmail.com>
2022-05-03 05:43:14 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,7 @@
 #include <stdio.h>
-
-/**
- * main - Entry point
- *
- * Return: Always 0 (Success)
- */
-int main(void)
+int main()
 {
-	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
+	puts("\"Programming is like buildinga multilingual puzzle");
+	return(0);
 }
+
