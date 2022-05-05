9968074bc2cad1df3cbfeef6292dd4088792fad3
kenkomu <komukenneth216@gmail.com>
2022-05-05 10:51:48 -0700

    0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -13,15 +13,15 @@
 	n = rand() - RAND_MAX / 2;
 	if (n>0)
     {
-        printf("is positive");
+        printf("%d is positive",n);
     }
     if (n=0)
     {
-        printf("is zero");
+        printf("%d is zero",n);
     }
     if (n<0)
     {
-        printf("is negative");
+        printf("%d is negative",n);
     }
     return(0);
  }
