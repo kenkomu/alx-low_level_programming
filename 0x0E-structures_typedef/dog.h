fb137de92927fe0b988c826dda0415cb15ef0cb4
kenkomu <komukenneth216@gmail.com>
2022-05-24 05:27:41 -0700

    dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,26 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - structure named dog
+ * @name: character input
+ * @age: float input
+ * @owner: character input
+ * Discription: struct named dog
+ */
+
 struct dog
 {
-	char* name;
-	char* owner;
+	char *name;
+	char *owner;
 	float age;
 };
+
+typedef struct dog dog_t;
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+
+#endif
