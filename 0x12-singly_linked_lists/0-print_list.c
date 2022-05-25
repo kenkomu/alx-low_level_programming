c6e182607e3485a0240fcab5a5472abf537e2d54
kenkomu <komukenneth216@gmail.com>
2022-05-25 00:01:02 -0700

    0-print_list.c

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,7 +1,9 @@
 #include "lists.h"
+#include <stdio.h>
+#include <stddef.h>
 /**
  * function that prints all the elements of a list_t list
- * @l: list_t
+ * @h: list_t
  * Return: the number of nodes
  */
 size_t print_list(const list_t *h)
--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,11 +1,14 @@
 #ifndef _LIST_H_
 #define _LIST_H_
-
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
 
-int putchar(const char *string);
+typedef struct list_s
+{
+	char *str;
+	unsigned int len;
+	struct list_s *next;
+} list_t;
+
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
 list_t *add_node(list_t **head, const char *str);
