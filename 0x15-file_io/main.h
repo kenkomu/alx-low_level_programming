46db000933fe79017576bda3be3c4ea06dbd04f0
kenkomu <komukenneth216@gmail.com>
2022-06-16 14:06:52 -0700

    program that copies the content of a file to another file.

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -11,6 +11,11 @@ int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
+void _errorWriteDisplay(char *prmFileName);
+void _errorReadDisplay(char *prmFileName);
+void _checkFile(char *prmDestFileName, char *prmSourceFileName);
+void _copy(char *prmFileNameDest, char *prmFileNameSource);
+
 
 #endif
 
