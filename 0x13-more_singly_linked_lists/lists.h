2dceb091811fdb478c771f43058a5c4f09d2a3aa
kenkomu <komukenneth216@gmail.com>
2022-06-16 12:47:10 -0700

    Header file

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -1,12 +1,20 @@
-#ifndef LISTS_H_
-#define LISTS_H_
+#ifndef LISTS_H
+#define LISTS_H
+
+#include <stdlib.h>
+#include <string.h>
 #include <stdio.h>
-typedef struct listint_t
+
+/**
+ * listint - singly linked list
+ * @n: the integer elements
+ * @next: pointer to the next node
+ */
+typedef struct listint
 {
-	char *str;
-	unsigned int len;
-	struct listint_t *next;
-}listint_t;
+	int n;
+	struct listint *next;
+} listint_t;
 
 size_t print_listint(const listint_t *h);
 size_t listint_len(const listint_t *h);
@@ -21,8 +29,5 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n);
 int delete_nodeint_at_index(listint_t **head, unsigned int index);
 listint_t *reverse_listint(listint_t **head);
 size_t print_listint_safe(const listint_t *head);
-size_t free_listint_safe(listint_t **h);
-listint_t *find_listint_loop(listint_t *head);
 
 #endif
-
