d3372ba3d9e25b54033c6eb1392e2740a814418d
kenkomu <komukenneth216@gmail.com>
2022-06-16 14:09:13 -0700

    My header file

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -7,15 +7,16 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 
+#define EXIT_WRONG_PARAMETER 97
+#define EXIT_READ_ERROR 98
+#define EXIT_WRITE_ERROR 99
+#define EXIT_CLOSE_ERROR 100
+#define BUFFER_SIZE 1024
+#define DEFAULT_WRITE_RIGHTS 0664
 int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
-void _errorWriteDisplay(char *prmFileName);
-void _errorReadDisplay(char *prmFileName);
-void _checkFile(char *prmDestFileName, char *prmSourceFileName);
-void _copy(char *prmFileNameDest, char *prmFileNameSource);
-
 
 #endif
 
