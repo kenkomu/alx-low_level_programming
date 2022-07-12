30f47c53e74f9a2f677f018f7c307d48b028a8a7
kenkomu <komukenneth216@gmail.com>
2022-07-12 03:55:52 -0700

    adds a new node at the beginning of a dlistint list

---
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -0,0 +1,48 @@
+#include "lists.h"
+/**
+ * _createnode - creates a new node
+ * @prmNbr: name
+ * Return: new element
+ */
+
+dlistint_t *_createNode(const int prmNbr)
+{
+	dlistint_t *new = malloc(sizeof(dlistint_t));
+
+	if (new == NULL)
+		return (NULL);
+	new -> n = prmNbr;
+	new -> next = NULL;
+	new -> prev = NULL;
+
+	return(new);
+}
+
+/**
+ * add_dnodeint - adds a new node at the beginning of dlistint_t list
+ * head: pointer to the last element
+ * n: number
+ *
+ * Return: the new last element
+ */
+
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
+{
+	dlistint_t *new;
+	if (head == NULL)
+		return (NULL);
+
+	new = _createNode(n);
+	if (new == NULL)
+		return (NULL);
+	if (*head == NULL)
+		*head = new;
+	else
+	{
+		(*head) -> prev = new;
+		new -> next = *head;
+		*head = new;
+	}
+	return (new);
+}
+
