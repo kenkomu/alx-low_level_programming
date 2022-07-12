749dd63b4317267befc40fcd57e9f90597412c2d
kenkomu <komukenneth216@gmail.com>
2022-07-12 03:28:27 -0700

    Number of elements in a list

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -1,10 +1,10 @@
 #include "lists.h"
 /**
- * dlistint_len - return the number of elements in a linked list
+ * dlistint_len -  the number of elements in a dlistint_t linked list
  * @h: list to browse
  * return: the size of list
  */
-size_t dlistint_len(const dlistint_t *h);
+size_t dlistint_len(const dlistint_t *h)
 {
 	if (h == NULL)
 		return (0);
