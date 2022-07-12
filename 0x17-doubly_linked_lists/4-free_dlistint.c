2552f9945ddcfed85cfdca2f4ac4ff50d98809ed
kenkomu <komukenneth216@gmail.com>
2022-07-12 04:37:17 -0700

    frees a dlistint list

---
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,14 @@
+#include "lists.h"
+/**
+ * free_dlistint - frees a dlistint_t
+ * @head: first element
+ * Return: frees a dlistint list
+ */
+
+void free_dlistint(dlistint_t *head)
+{
+	if (head == NULL)
+		return
+	free_dlistint(head->next);
+	free(head);
+}
