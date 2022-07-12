ddd9a2466c1b0273eb0ec7005c717bfa754e9f1f
kenkomu <komukenneth216@gmail.com>
2022-07-12 09:27:42 -0700

    sum of all the data of a linked list

---
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+/**
+ * sum_dlistint - sum of all the data of a dlistint list
+ * @head: first element
+ * Return: sum of tehe linked list
+ */
+
+int sum_dlistint(dlistint_t *head);
+{
+	int sum = 0;
+
+	if (head == NULL)
+		return (0);
+	while (head != NULL)
+	{
+		sum += head->n;
+		head = head->next;
+	}
+	return (sum);
+}
