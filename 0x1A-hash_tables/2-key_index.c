0665bc760d33938088167da1dd8e0488ba5cdec2
kenkomu <komukenneth216@gmail.com>
2022-08-01 11:52:27 +0300

    2-key_index.c

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -1,12 +1,14 @@
 #include "hash_tables.h"
 /**
  * key_index - function that returns index
- * 
- * @key 
- * @size 
- * Return: index 
+ *
+ * @key : key of array
+ * @size : size of the array
+ * Return: index
  */
-unsigned long int key_index(const unsigned char *key, unsigned long int size)
+unsigned long int key_index(
+	const unsigned char *key,
+	unsigned long int size)
 {
-    return (hash_djb2(key)% size);
+	return (hash_djb2(key) % size);
 }
