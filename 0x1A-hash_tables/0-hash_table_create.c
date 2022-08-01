1c068b50bbbd690e22273f4effd86ee53e7a7317
kenkomu <komukenneth216@gmail.com>
2022-08-01 10:56:45 +0300

    0-hash_table_create.c

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -1,20 +1,22 @@
 #include "hash_tables.h"
 /**
  * hash_table_create - function that create the hash table
- * 
+ *
  * @size - the size of the array
- * 
+ *
  * Return: pointer to a naw hash table or NULL if fail
  */
 
-hash_table_t *hash_table_create(unsigned long int size){
-    hash_table_t *ht;
-    ht = malloc (sizeof(hash_table_t));
-    if (ht = NULL)
-        return NULL;
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *ht;
 
-    ht->size = size;
-    ht->array = calloc(sizeof(hash_node_t *), ht->size);
+	ht = malloc(sizeof(hash_table_t));
+	if (ht == NULL)
+		return (NULL);
+
+	ht->size = size;
+	ht->array = calloc(sizeof(hash_node_t *), ht->size);
 	if (ht->array == NULL)
 	{
 		free(ht);
