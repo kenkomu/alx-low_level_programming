9295b5bf96895f30dce291e666bb40c1e6343d3f
kenkomu <komukenneth216@gmail.com>
2022-08-01 10:50:03 +0300

    0-hash_table_create.c

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -11,6 +11,14 @@ hash_table_t *hash_table_create(unsigned long int size){
     hash_table_t *ht;
     ht = malloc (sizeof(hash_table_t));
     if (ht = NULL)
-        return NULL
+        return NULL;
 
+    ht->size = size;
+    ht->array = calloc(sizeof(hash_node_t *), ht->size);
+	if (ht->array == NULL)
+	{
+		free(ht);
+		return (NULL);
+	}
+	return (ht);
 }
