1a8e284fc7207fd9e1482d1534f16452e326ace7
kenkomu <komukenneth216@gmail.com>
2022-11-05 07:52:44 +0300

    0-linear.c

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -9,20 +9,30 @@
  *
  * Return: the first index where value is located
  */
-int linear_search(int *array, size_t size, int value);
+int linear_search(int *array, size_t size, int value)
 {
-	size_t loop;
+	int *array[9];
 
 	if (array == NULL)
 	{
 		return (-1);
 	}
-	for (loop = 0; loop < size; loop++)
-	{
-		printf("Value checked array[%ld] = [%d]\n")
-		if (array(loop) == value)
-			return (array(loop));
-	}
-
-	return (-1);
 }
+/**
+ * main - Entry point
+ *
+ * Return: Always EXIT_SUCCESS
+ */
+int main(void)
+{
+	int array[] = {
+		10, 1, 42, 3, 4, 42, 6, 7, -1, 9
+	};
+	size_t size = sizeof(array) / sizeof(array[0]);
+
+	printf("Found %d at index: %d\n\n", 3, linear_search(array, size, 3));
+	printf("Found %d at index: %d\n\n", 42, linear_search(array, size, 42));
+	printf("Found %d at index: %d\n", 999, linear_search(array, size, 999));
+	return (EXIT_SUCCESS);
+}
+
