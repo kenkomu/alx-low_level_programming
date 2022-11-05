c7570ba381d9c93ba08478bf8d041cfd4374837e
kenkomu <komukenneth216@gmail.com>
2022-11-05 07:31:20 +0300

    header file

---
+++ 0x1E-search_algorithms/search_algos.h
@@ -0,0 +1,10 @@
+#ifndef SEARCH_ALGOS_H_
+#define SEARCH_ALGOS_H_
+#include <stdio.h>
+#include <stdlib.h>
+
+int linear_search(int *array, size_t size, int value);
+int binary_search(int *array, size_t size, int value);
+
+
+#endif
