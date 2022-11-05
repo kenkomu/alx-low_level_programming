7e9fbf93a68cf66013156c25be795f52104965b3
kenkomu <komukenneth216@gmail.com>
2022-11-05 07:55:10 +0300

    search_algos.h

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -1,7 +1,9 @@
-#ifndef SEARCH_ALGOS_H_
-#define SEARCH_ALGOS_H_
+#ifndef SEARCH_ALGOS_H
+#define SEARCH_ALGOS_H
+
 #include <stdio.h>
 #include <stdlib.h>
+
 /**
  * struct listint_s - singly linked list
  *
@@ -10,6 +12,7 @@
  * @next: Pointer to the next node
  *
  * Description: singly linked list node structure
+ * for Holberton project
  */
 typedef struct listint_s
 {
@@ -18,8 +21,12 @@ typedef struct listint_s
 	struct listint_s *next;
 } listint_t;
 
-int linear_search(int *array, size_t size, int value);
-int binary_search(int *array, size_t size, int value);
 
+int binary_search(int *array, size_t size, int value);
+int exponential_search(int *array, size_t size, int value);
+int interpolation_search(int *array, size_t size, int value);
+listint_t *jump_list(listint_t *list, size_t size, int value);
+int jump_search(int *array, size_t size, int value);
+int linear_search(int *array, size_t size, int value);
 
 #endif
