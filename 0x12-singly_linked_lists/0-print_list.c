160c02866b220d074fd55513aeb9d2d2c33e5f2c
kenkomu <komukenneth216@gmail.com>
2023-02-07 20:20:55 +0300

    0-print_list.c

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -9,21 +9,17 @@
  */
 size_t print_list(const list_t *h)
 {
-    size_t count = 0;
-    const list_t *current = h;
+	size_t count = 0;
+	const list_t *current = h;
 
-    while (current != NULL)
-    {
-        if (current->str == NULL)
-            printf("[%lu] (nil)\n", current->len);
-        else
-            printf("[%lu] %s\n", current->len, current->str);
-
-        current = current->next;
-        count++;
-    }
-
-    return count;
+		while (h != NULL)
+	{
+		if (h->str == NULL)
+			printf("[0] (nil)\n");
+		else
+			printf("[%d] %s\n", h->len, h->str);
+		h = h->next;
+		count++;
+	}
+	return (count);
 }
-
-
