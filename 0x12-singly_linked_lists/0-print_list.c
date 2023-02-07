758c9278fe3fbe53d98792b075ff7d5cdfd40a23
kenkomu <komukenneth216@gmail.com>
2023-02-07 09:27:29 +0300

    function that prints all the elements of a list_t list

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -7,3 +7,13 @@
  * @return size_t 
  */
 size_t print_list(const list_t *h);
+new = malloc(sizeof(list_t));
+    if (new == NULL)
+    {
+        printf("Error\n");
+        return (1);
+    }
+new->str = strdup("Hello");
+new->len = 5;
+new->next = head;
+head = new;
