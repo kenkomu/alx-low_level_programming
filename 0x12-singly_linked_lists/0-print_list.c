ef2f28b84be7066d31a62adea1395746be40222b
kenkomu <komukenneth216@gmail.com>
2023-02-07 20:16:19 +0300

    function that prints all the elementsof a list_t list

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,5 +1,6 @@
-#include "lists.h"
 #include <stdio.h>
+#include "lists.h"
+
 /**
  * print_list - print elements of a list_t list
  * @h: pointer to the head of the list
@@ -8,30 +9,21 @@
  */
 size_t print_list(const list_t *h)
 {
-    while(h != NULL){
-        printf("%s\n", h->str);
-        h = h->next;
+    size_t count = 0;
+    const list_t *current = h;
+
+    while (current != NULL)
+    {
+        if (current->str == NULL)
+            printf("[%lu] (nil)\n", current->len);
+        else
+            printf("[%lu] %s\n", current->len, current->str);
+
+        current = current->next;
+        count++;
     }
+
+    return count;
 }
-int main (){
-    struct list_s* head = NULL;
-    struct list_s* first = NULL;
-    struct list_s* last = NULL;
 
-    head = malloc(sizeof(list_t));
-    first = malloc (sizeof(list_t));
-    last = malloc(sizeof(list_t));
 
-    head->str = "ken";
-    head->next = first;
-
-    first->str = "komu";
-    first->next = last;
-
-    last->str = "NJoroge";
-    last->next = NULL;
-
-    print_list(head);
-
-    return 0;
-}
