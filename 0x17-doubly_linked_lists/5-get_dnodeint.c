a0ebe18879664df0175e55156fc60055e2783b8d
kenkomu <komukenneth216@gmail.com>
2023-06-05 13:02:44 +0300

    5-get_dnodeint.c

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -7,18 +7,16 @@
  */
 dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 {
-dlistint_t *current = head;
 
-    while (current != NULL && index > 0) {
+    dlistint_t* current = head;
+    
+    while(current != NULL && index > 0)
+    {
         current = current->next;
         index--;
     }
+    if (current = NULL)
+        return (NULL);
 
-    if (current == NULL) {
-        
-        return NULL;
-    }
-
-    return current;
-
+    return (current);
 }
