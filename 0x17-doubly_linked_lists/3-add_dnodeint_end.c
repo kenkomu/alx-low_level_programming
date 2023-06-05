4c2742f1c7ba702a24adf639f1e8eade402b45a8
kenkomu <komukenneth216@gmail.com>
2023-06-05 10:32:32 +0300

    3-add_dnodeint_end.c

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -11,6 +11,11 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 {
 	dlistint_t *newNode = malloc(sizeof(dlistint_t));
 
+	if (newNode == NULL)
+	{
+		return (NULL);
+	}
+
 
 	dlistint_t *temp = *head;
 
