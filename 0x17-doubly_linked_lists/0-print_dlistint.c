13ab30c6725bf5efc56077311b0599bbfe508c26
kenkomu <komukenneth216@gmail.com>
2023-06-05 07:09:35 +0300

    0-print_dlistint.c

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -1,4 +1,3 @@
-
 #include "lists.h"
 /**
  * print_dlistint - prints all the elements of a dlistint_t list
@@ -9,11 +8,11 @@
  */
 size_t print_dlistint(const dlistint_t *h)
 {
+	size_t size = 0;
+	dlistint_t *traverser = h;
+
 	if (h == NULL)
 		return (0);
-
-	dlistint_t *traverser = h;
-	size_t size = 0;
 
 	while (traverser != NULL)
 	{
